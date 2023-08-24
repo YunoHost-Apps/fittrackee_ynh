42be0a827937308afc6830dfa57306f6c9427ecf
Thomas <51749973+Thovi98@users.noreply.github.com>
2023-08-24 13:29:11 +0200

    upgrade to 0.7.22

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-fittrackee_version="0.7.21"
+fittrackee_version="0.7.22"
 
 #=================================================
 # PERSONAL HELPERS
