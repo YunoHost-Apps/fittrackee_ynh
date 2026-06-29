27822e5bd690712aa37c96cdd6b1de5b31e216aa
orhtej2 <2871798+orhtej2@users.noreply.github.com>
2026-06-29 14:43:31 +0200

    ff

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,6 +6,6 @@
 
 fixupx64numpy() {
      if ! grep -qE '^flags.* (sse4|LSE)' /proc/cpuinfo; then
-       ynh_setup_source --dest_dir="$install_dir/venv/lib/python3.11/site-packages" --source_id="x64_bookworm"
+       ynh_setup_source --dest_dir="$install_dir/venv" --source_id="x64_bookworm"
      fi
 }
